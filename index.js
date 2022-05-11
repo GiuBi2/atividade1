e49dd5ca366427046ab82f99b5d42427ae76d178
Giulia Bianca <86010339+GiuBi2@users.noreply.github.com>
2022-05-11 21:15:28 +0000

    criei rotas para empresas

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 var express = require("express");
 var app = express();
 var {usuario} = require("./models");
+var {empresa} = require("./models");
 
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }))
-app.get("/", async function(req, res){
+//*usuario
+app.get("/usuarios", async function(req, res){
   var resultado = await usuario.findAll(req.body);
   res.json(resultado);
 });
-app.get("/:id", async function(req, res){
+
+app.get("/usuarios:id", async function(req, res){
   var resultado = await usuario.findOne({where:{id: req.params.id}});
   res.json(resultado);
 })
-app.post("/",  async function(req, res){
+app.post("/usuarios",  async function(req, res){
  var resultado = await usuario.create(req.body);
   res.json(resultado);
 });
 
-app.put("/:id", async function(req, res){
+app.put("/usuarios:id", async function(req, res){
   var resultado = await usuario.update(req.body, { where:{ id:req.params.id }});
   res.json(resultado);
 });
 
-app.delete("/:id", async function(req, res){
+app.delete("/usuarios:id", async function(req, res){
   var resultado = await usuario.destroy({where:{id: req.params.id}});
+  res.json(resultado);
+});
+
+//*empresa
+app.get("/empresas", async function(req, res){
+  var resultado = await empresa.findAll(req.body);
+  res.json(resultado);
+}); 
+
+app.get("/empresas/:id", async function(req, res){
+  var resultado = await empresa.findOne({where:{id: req.params.id}});
+  res.json(resultado);
+})
+app.post("/empresas",  async function(req, res){
+ var resultado = await empresa.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/empresas/:id", async function(req, res){
+  var resultado = await empresa.update(req.body, { where:{ id:req.params.id }});
+  res.json(resultado);
+});
+
+app.delete("/empresas/:id", async function(req, res){
+  var resultado = await empresa.destroy({where:{id: req.params.id}});
   res.json(resultado);
 });
 
